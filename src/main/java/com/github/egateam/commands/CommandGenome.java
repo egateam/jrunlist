26e9fc77fa73e3af16c5c7609e7d4294f8f1ac12
wang-q <wang-q@outlook.com>
2016-04-24 16:15:14 +0800

    ReadSizes.read()

--- src/main/java/com/github/egateam/commands/CommandGenome.java
+++ src/main/java/com/github/egateam/commands/CommandGenome.java
@@ -3,14 +3,17 @@ package com.github.egateam.commands;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.Parameters;
+import com.github.egateam.IntSpan;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Parameters(commandDescription = "Convert chr.size to runlists")
 public class CommandGenome {
 
-    @Parameter(description = "<infile>", converter = FileConverterIn.class)
+    @Parameter(description = "<infile>", converter = FileConverterIn.class, required = true)
     private List<File> files;
 
     @Parameter(names = {"--outfile", "-o"}, description = "Output filename. [stdout] for screen.")
@@ -19,7 +22,7 @@ public class CommandGenome {
     @Parameter(names = {"--remove", "-r"}, description = "Remove 'chr0' from chromosome names.")
     private Boolean remove = false;
 
-    @Parameter(names = {"--help"}, help = true)
+    @Parameter(names = "--help", help = true)
     private boolean help;
 
     private void validateArgs() {
@@ -37,7 +40,17 @@ public class CommandGenome {
 
         File inFile = files.get(0);
 
+        Map<String, Integer> lengthOf = new ReadSizes(inFile, remove).read();
+        Map<String, IntSpan> setOF = new HashMap<>();
+
+        for ( Map.Entry<String, Integer> entry : lengthOf.entrySet() ) {
+            IntSpan set = new IntSpan();
+            set.addPair(1, entry.getValue());
+            setOF.put(entry.getKey(), set);
+        }
+
         System.err.printf("infile is: %s%n", inFile);
+        System.err.printf("number of entries is: %d%n", setOF.size());
         System.err.printf("output is: %s, remove is: %s%n", outfile, remove);
         System.out.print("Genome() success\n");
     }
---
+++ src/main/java/com/github/egateam/commands/ReadSizes.java
@@ -0,0 +1,38 @@
+package com.github.egateam.commands;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("WeakerAccess")
+public class ReadSizes {
+    private File file;
+    private boolean remove;
+
+    public ReadSizes(File file, boolean remove) {
+        this.file = file;
+        this.remove = remove;
+    }
+
+    public Map<String, Integer> read() {
+        HashMap<String, Integer> lengthOf = new HashMap<>();
+
+        try ( BufferedReader reader = new BufferedReader(new FileReader(file)) ) {
+            String line;
+            while ( (line = reader.readLine()) != null ) {
+                line = line.trim();
+                String[] fields = line.split("\\t");
+                if ( fields.length == 2 ) {
+                    if ( remove ) {
+                        fields[0] = fields[0].replaceFirst("chr0?", "");
+                    }
+                    lengthOf.put(fields[0], Integer.parseInt(fields[1]));
+                }
+            }
+        } catch ( Exception err ) {
+            err.printStackTrace();
+        }
+
+        return lengthOf;
+    }
+}
