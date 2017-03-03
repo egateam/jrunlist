1d8976f4df6991f7e4574dcaa9a0bb5974f6d4bf
wang-q <wang-q@outlook.com>
2017-03-03 13:58:52 +0800

    dazzname contains "/"

--- src/test/java/com/github/egateam/jrunlist/commands/CoverTest.java
+++ src/test/java/com/github/egateam/jrunlist/commands/CoverTest.java
@@ -18,10 +18,10 @@ import java.io.PrintStream;
 
 public class CoverTest {
     // Store the original standard out before changing it.
-    private final PrintStream originalStdout = System.out;
-    private final PrintStream originalStderr = System.err;
-    private ByteArrayOutputStream stdoutContent = new ByteArrayOutputStream();
-    private ByteArrayOutputStream stderrContent = new ByteArrayOutputStream();
+    private final PrintStream           originalStdout = System.out;
+    private final PrintStream           originalStderr = System.err;
+    private       ByteArrayOutputStream stdoutContent  = new ByteArrayOutputStream();
+    private       ByteArrayOutputStream stderrContent  = new ByteArrayOutputStream();
 
     @BeforeMethod
     public void beforeTest() {
@@ -41,14 +41,25 @@ public class CoverTest {
 
     @Test(description = "Test command with S288c.txt")
     public void testExecute() throws Exception {
-        String fileName = Utils.expendResource("S288c.txt");
-        String[] args = {"cover", fileName, "--outfile", "stdout"};
+        String   fileName = Utils.expendResource("S288c.txt");
+        String[] args     = {"cover", fileName, "--outfile", "stdout"};
         Cli.main(args);
 
         Assert.assertEquals(this.stdoutContent.toString().split("\r\n|\r|\n").length, 3, "line count");
         Assert.assertFalse(this.stdoutContent.toString().contains("S288c"), "species name");
         Assert.assertFalse(this.stdoutContent.toString().contains("1-100"), "merged");
         Assert.assertTrue(this.stdoutContent.toString().contains("1-150"), "covered");
+    }
+
+    @Test(description = "Test command with dazzname.txt")
+    public void testExecute2() throws Exception {
+        String   fileName = Utils.expendResource("dazzname.txt");
+        String[] args     = {"cover", fileName, "--outfile", "stdout"};
+        Cli.main(args);
+
+        Assert.assertEquals(this.stdoutContent.toString().split("\r\n|\r|\n").length, 2, "line count");
+        Assert.assertTrue(this.stdoutContent.toString().contains("infile_0/1/0_514"), "chr name");
+        Assert.assertTrue(this.stdoutContent.toString().contains("19-499"), "covered");
     }
 
     @AfterMethod
